9e2dddcdac910f8e5685563c3f7af7bee89a1ac7
MUHAMMAD NAZMI HAKIM BIN MOHD AZAM <s75948@ocean.umt.edu.my>
2025-12-03 22:51:46 +0800

    Delete form-validation.js

--- form-validation.js
+++
@@ -1,9 +0,0 @@
-
-function validateForm() {
-    let fname = document.forms["myForm"]["fname"].value;
-    if (fname == "") {
-        alert("First name must be filled out");
-        return false;
-    }
-    return true;
-}
